93751611f05eadafc94472c090926fbe2757f5f4
Tom <blak1002@gmail.com>
2022-03-18 19:10:59 +0900

    MVC_Pattern Practice

---
+++ app/src/main/java/com/example/viewpattern/data/Book.kt
@@ -0,0 +1,3 @@
+package com.example.viewpattern.data
+
+data class Book(var name:String, var author:String)
---
+++ app/src/main/java/com/example/viewpattern/utils/Constansts.kt
@@ -0,0 +1,5 @@
+package com.example.viewpattern.utils
+
+object Constansts {
+    const val TAG: String = "로그"
+}
